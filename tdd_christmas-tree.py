57e473e45f55551b927866c56fd268982e9e4064
joh <joerg-haas@t-online.de>
2023-12-20 14:51:53 +0100

    fertig

--- tdd_christmas-tree.py
+++ tdd_christmas-tree.py
@@ -1,21 +1,59 @@
 '''
-example für TDD: fizz buzz challange:
-schreibe eine Funktion, die eine pos int n übernimmt und einen str returned
-fizz wenn n Vielfaches von 3 ist
-buzz wenn n Vielfaches von 5 ist
-fizzbuzz wenn n Vielfaches von 3 und 5 ist ()
-sonst ""
+christmas tree:
+bel Größe mit mittigem Stamm der Länge 1
+z.B. tree(1):
+*
+|
+
+tree(2):
+ *
+***
+ |
+
 '''
 
 import pytest
 
-def get_tree(n):
-    return ''
+def getCrownLine(index, height):
+    anzX = 2 * index + 1
+    maxX = 2 * height - 1
+    anzSpaces = int((maxX - anzX) / 2)
+    crownLine = anzSpaces*' '  + anzX*'X' +  anzSpaces*' ' + '\n'
+    return crownLine
 
-class Test_get_tree():
-    def test_1(self):
-        ...
+def getTrunkString(height):
+    anzTrunkSpaces = height - 1
+    trunkString = anzTrunkSpaces * ' ' +'|' + anzTrunkSpaces * ' '  +  '\n'
+    return trunkString
 
+def get_tree(height):
+    treeStr = ''
+    #only int > 0
+    if height:
+        #Kronenzeilen:
+        for index in range(height):
+            treeStr += getCrownLine(index, height)
+        #Stammzeile
+        treeStr += getTrunkString(height)
+    return treeStr
 
-# if __name__ == '__main__':
-#     unittest.main()
+# ------------------ tests ----------------------
+
+def test_Anz_lines0():
+    assert get_tree(0) == '', 'Anzahl null wird falsch bedient'
+
+def test_anz_lines_gesamt():
+    '''test Gesamtlänge incl Stammzeile'''
+    for i in range(1,4+1):
+        treeStr = get_tree(i)
+        assert len(get_tree(i).splitlines()) - 1 == i, f'Längenfehler bei Länge {i}'
+
+def test_trunk():
+    assert '|' in getTrunkString(2), 'no | in trunk string'
+
+def test_anz_ges():
+    for i in range(1,4+1):
+        assert len(get_tree(i)) == 2*i**2+2*i
+
+if __name__ == '__main__':
+    print(get_tree(20))
